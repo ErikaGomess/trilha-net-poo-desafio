6566619dbd4d5e90c05e948dbe356bd09314970f
Erika de Jesus Gomes <erikagomes1407@gmail.com>
2025-07-22 20:26:21 -0300

    feat: implementar modelo de smartphone com classes Nokia e iPhone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Testando smartphone Nokia:");
+        Smartphone nokia = new Nokia("11999999999", "Nokia 3310", "123456789012345", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Testando smartphone iPhone:");
+        Smartphone iphone = new Iphone("11888888888", "iPhone 13", "987654321098765", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
